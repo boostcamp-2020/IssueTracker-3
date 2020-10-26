69c5b922e888cec8cbc7dd6f45922908670586b0
Minkwan Song <minkwan1120@naver.com>
2020-10-27 08:18:14 +0900

    Style : 프로젝트 초기 세팅 및 주석 제거

--- iOS/IssueTracker/IssueTrackerUITests/IssueTrackerUITests.swift
+++ iOS/IssueTracker/IssueTrackerUITests/IssueTrackerUITests.swift
@@ -10,14 +10,15 @@ import XCTest
 class IssueTrackerUITests: XCTestCase {
 
     override func setUpWithError() throws {
+      
     }
 
     override func tearDownWithError() throws {
-
+      
     }
 
     func testExample() throws {
-
+      
     }
 
     func testLaunchPerformance() throws {
