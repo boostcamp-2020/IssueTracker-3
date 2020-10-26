f446932acb4b77bf28b0ba9e46a93e98c186edf5
park <wogus3602@naver.com>
2020-10-27 00:12:08 +0900

    Test : auto unit test

--- iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -18,9 +18,14 @@ class IssueTrackerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testTrueExample() throws {
         XCTAssertTrue(true)
     }
+
+    func testFalseExample() throws {
+        XCTAssertFalse(false)
+    }
+
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
