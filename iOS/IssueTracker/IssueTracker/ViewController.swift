c76ef1f758efa582dc9bb86987c1605cda88de5c
park <wogus3602@naver.com>
2020-10-26 19:32:25 +0900

    Add : .gitignore

---
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by ParkJaeHyun on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
