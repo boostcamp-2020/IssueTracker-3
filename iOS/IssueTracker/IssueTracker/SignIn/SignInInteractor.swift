254d5587aeb5c20587fc15966683b6180fa45fad
Minkwan Song <minkwan1120@naver.com>
2020-10-27 19:28:27 +0900

    feat : SignInInteractor class 및 SignInInteractorProtocol 선언

---
+++ iOS/IssueTracker/IssueTracker/SignIn/SignInInteractor.swift
@@ -0,0 +1,19 @@
+//
+//  SignInInteractor.swift
+//  IssueTracker
+//
+//  Created by 송민관 on 2020/10/27.
+//
+
+import Foundation
+
+protocol SignInInteractorProtocol {
+    
+}
+
+final class SignInInteractor: SignInInteractorProtocol {
+    
+    // Id pw -> network
+    // Id pw 검증 받아서 -> 결과값
+    
+}
