a6e8535347990cd346c4f53b83027c7dde208899
Minkwan Song <minkwan1120@naver.com>
2020-10-27 18:09:23 +0900

    feat : SignInViewController 함수 선언

--- iOS/IssueTracker/IssueTracker/SignIn/SignInViewController.swift
+++ iOS/IssueTracker/IssueTracker/SignIn/SignInViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    @IBAction func SignInTouched(_ sender: UIButton) {
+        
+    }
+    
+    // TODO: 로그인 실패/성공 : toast
+    
+    @IBAction func SignInWithGitHubTouched(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func SignInWithAppleTouched(_ sender: UIButton) {
+        
     }
 }
