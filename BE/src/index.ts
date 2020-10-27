8b0e84c634cef7610e81aac5c1e59968746b9548
ji <ji_meng@naver.com>
2020-10-27 17:50:25 +0900

    chore: 백엔드 개발환경 설정 typescript + nodejs + express + eslint + pritter
     #28

---
+++ BE/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+class App {
+  public application: express.Application;
+
+  constructor() {
+    this.application = express();
+  }
+}
+const app = new App().application;
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("start");
+});
+app.listen(4000, () => console.log("start"));
