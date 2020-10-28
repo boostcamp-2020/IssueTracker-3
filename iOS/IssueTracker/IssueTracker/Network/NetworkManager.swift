a0d58780723dbb39f7fa95a4e434a427daa71339
park <wogus3602@naver.com>
2020-10-28 15:52:50 +0900

    feat : NetworkManager Class 생성

--- iOS/IssueTracker/IssueTracker/Network/NetworkManager.swift
+++ iOS/IssueTracker/IssueTracker/Network/NetworkManager.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkManager {
+    let session: SessionManager
+
+    init(sessionManager: SessionManager) {
+        self.session = sessionManager
+    }
+
+    // TODO: EndPoint 만들기
+    func request(url: String, completionHandler: @escaping (Data?) -> Void) {
+
+        let alamofire = session.request(url,
+                                        method: .get,
+                                        parameters: nil,
+                                        encoding: URLEncoding.default,
+                                        headers: nil,
+                                        interceptor: nil,
+                                        requestModifier: nil)
+
+        alamofire.response { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(value)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+protocol SessionManager {
+    func request(_ convertible: URLConvertible,
+                 method: HTTPMethod,
+                 parameters: Parameters?,
+                 encoding: ParameterEncoding,
+                 headers: HTTPHeaders?,
+                 interceptor: RequestInterceptor?,
+                 requestModifier: Session.RequestModifier?) -> DataRequest
+}
+
+extension Session: SessionManager {
+
+}
