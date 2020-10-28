635bca8086567eb8efab6193ad7a285a84f87db6
park <wogus3602@naver.com>
2020-10-29 00:12:31 +0900

    style : 불필요한 코드 제거

--- iOS/IssueTracker/IssueTracker/SceneDelegate.swift
+++ iOS/IssueTracker/IssueTracker/SceneDelegate.swift
@@ -14,24 +14,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-      
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-      
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-       
-    }
 }
