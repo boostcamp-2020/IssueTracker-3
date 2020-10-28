348d08b60dc63d28130f1047c2dcd6eb46ac7d94
ji <ji_meng@naver.com>
2020-10-28 18:13:02 +0900

    fix: 머지 충돌 수정 기존의 index.ts의 기능을 app.ts가 수행하게되어 충돌이 발생하여 수정

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -1,9 +1,22 @@
 import express, { Application } from "express";
-
+import passport from "passport";
+import session from "express-session";
+import bodyParser from "body-parser";
 import helmet from "helmet";
+
+import index from "./route/index";
+import auth from "./route/auth";
+import milestone from "./route/milestone";
+import label from "./route/label";
+import issue from "./route/issue";
+import event from "./route/event";
+
+import Passport from "./passport/passport";
 
 class App {
   private app: Application;
+
+  private passportConfig: Passport;
 
   constructor(private port?: number | string) {
     this.app = express();
@@ -19,12 +32,32 @@ class App {
 
   private settings() {
     this.app.set("port", process.env.PORT || this.port || 3000);
+    this.passportConfig = new Passport();
   }
 
   private middleware() {
     this.app.use(helmet());
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(passport.initialize());
+    this.app.use(
+      session({
+        secret: `@#@$MYSIGN#@$#$`,
+        resave: false,
+        saveUninitialized: true,
+      })
+    );
+    this.app.use(passport.session());
+    this.passportConfig.config();
   }
 
-  private routes() {}
+  private routes() {
+    this.app.use("/", index);
+    this.app.use("/auth", auth);
+    this.app.use("/milestone", milestone);
+    this.app.use("/label", label);
+    this.app.use("/issue", issue);
+    this.app.use("/event", event);
+  }
 }
 export default App;
