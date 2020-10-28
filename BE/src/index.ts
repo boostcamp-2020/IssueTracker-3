1b067d98cef470cd15325eeb5240baa082b6b796
mononomo <hostdog01@naver.com>
2020-10-28 14:43:09 +0900

    feat: milestone route 생성 milestone 관련 API 를 위한 route 를 만들었습니다.
     #12

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import index from "./route/index";
 import auth from "./route/auth";
+import milestone from "./route/milestone";
 
 class App {
   public application: express.Application;
@@ -12,6 +13,7 @@ class App {
 const app = new App().application;
 
 app.use("/", index);
-app.use("auth", auth);
+app.use("/auth", auth);
+app.use("/milestone", milestone);
 
 app.listen(4000, () => console.log("start"));
