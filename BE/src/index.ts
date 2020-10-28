97a772152e43ccbcbd36ac43458f78efc2232a91
mononomo <hostdog01@naver.com>
2020-10-28 14:52:20 +0900

    feat : issue route 생성 issue api 에 사용할 route 생성
     #12

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import index from "./route/index";
 import auth from "./route/auth";
 import milestone from "./route/milestone";
+import label from "./route/label";
+import issue from "./route/issue";
 
 class App {
   public application: express.Application;
@@ -15,5 +17,7 @@ const app = new App().application;
 app.use("/", index);
 app.use("/auth", auth);
 app.use("/milestone", milestone);
+app.use("/label", label);
+app.use("/issue", issue);
 
 app.listen(4000, () => console.log("start"));
