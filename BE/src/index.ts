c9e6771fe3b0fef8edca99cca0e1f8078ddc4933
mononomo <hostdog01@naver.com>
2020-10-28 17:45:20 +0900

    feat: authController 생성 auth route 적용할 메소드들을 따로 분히나 곳
     #12

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import passport from "passport";
+import session from "express-session";
+import bodyParser from "body-parser";
+
 import index from "./route/index";
 import auth from "./route/auth";
 import milestone from "./route/milestone";
 import label from "./route/label";
 import issue from "./route/issue";
 import event from "./route/event";
+
+import Passport from "./passport/passport";
 
 class App {
   public application: express.Application;
@@ -14,6 +20,20 @@ class App {
   }
 }
 const app = new App().application;
+const passportConfig: Passport = new Passport();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  session({
+    secret: `@#@$MYSIGN#@$#$`,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+passportConfig.config();
 
 app.use("/", index);
 app.use("/auth", auth);
--- BE/src/route/auth.ts
+++ BE/src/route/auth.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response, NextFunction } from "express";
+import authController from "../controller/authController";
 
 const router = express.Router();
 
-router.post("/login", (req: Request, res: Response, next: NextFunction) => {
-  res.send("login");
-});
+router.post("/login", authController.login);
+
 router.get("/logout", (req: Request, res: Response, next: NextFunction) => {
   res.send("logout");
 });
 router.post("/register", (req: Request, res: Response, next: NextFunction) => {
   res.send("register");
+});
+router.get("/users", (req: Request, res: Response, next: NextFunction) => {
+  res.send("users");
 });
 
 export = router;
