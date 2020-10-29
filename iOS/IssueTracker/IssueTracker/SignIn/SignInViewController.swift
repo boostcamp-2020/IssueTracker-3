2da2f4282ec842697e3edf8168561d79d8e90c41
park <wogus3602@naver.com>
2020-10-29 19:05:59 +0900

    feat : apple 로그인 press 후 데이터 받아오고 다음 화면 넘어가기 기능

--- iOS/IssueTracker/IssueTracker/SignIn/SignInViewController.swift
+++ iOS/IssueTracker/IssueTracker/SignIn/SignInViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 final class SignInViewController: UIViewController {
     
@@ -13,18 +14,69 @@ final class SignInViewController: UIViewController {
     
     @IBOutlet private weak var idTextField: UITextField!
     @IBOutlet private weak var pwTextField: UITextField!
-    
+    @IBOutlet private weak var signInWithAppleView: AppleSignInButton!
+
     // MARK: View Cycle
-    
+
+    var text: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSignInWithAppleView()
     }
-    
+
+    func configureSignInWithAppleView() {
+        signInWithAppleView.didCompletedSignIn = { [weak self] (user) in
+            // user.identityToken = JWT 토큰을 풀어서 name, email 가져오기, 서버로 보내기
+            // user.authorizationCode = 서버로 보낼 코드 // 5분만
+
+            self?.showResultViewController(userIdentifier: user.id,
+                                           givenName: user.firstName,
+                                           familyName: user.lastName,
+                                           email: user.email)
+        }
+    }
+
+    private func saveUserInKeychain(_ userIdentifier: String) {
+        do {
+            try KeychainItem(service: "com.example.apple-samplecode.juice",
+                             account: "userIdentifier").saveItem(userIdentifier)
+        } catch {
+            print("Unable to save userIdentifier to keychain.")
+        }
+    }
+
+    private func showResultViewController(userIdentifier: String?,
+                                          givenName: String?,
+                                          familyName: String?,
+                                          email: String?) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let loginViewController = storyboard.instantiateViewController(withIdentifier: "loginViewController")
+                as? ViewController else { return }
+
+        loginViewController.modalPresentationStyle = .formSheet
+        loginViewController.isModalInPresentation = true
+        self.present(loginViewController, animated: true, completion: nil)
+
+        DispatchQueue.main.async {
+            loginViewController.userIdentifierLabel.text = userIdentifier
+            if let givenName = givenName {
+                loginViewController.givenNameLabel.text = givenName
+            }
+            if let familyName = familyName {
+                loginViewController.familyNameLabel.text = familyName
+            }
+            if let email = email {
+                loginViewController.emailLabel.text = email
+            }
+        }
+
+    }
+
     // MARK: Action Functions
     
     @IBAction func signInTouched(_ sender: UIButton) {
         // TODO: idTextField.text / pwTextField.text => 인터렉터(검증)
-        
     }
     
     // TODO: 로그인 실패/성공 : toast
@@ -35,5 +87,18 @@ final class SignInViewController: UIViewController {
     
     @IBAction func signInWithAppleTouched(_ sender: UIButton) {
         // TODO: 애플 로그인 검증
+    }
+}
+
+extension SignInViewController: ASAuthorizationControllerDelegate {
+
+}
+
+extension SignInViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        guard let selfView = self.view.window else {
+            return ASPresentationAnchor()
+        }
+        return selfView
     }
 }
