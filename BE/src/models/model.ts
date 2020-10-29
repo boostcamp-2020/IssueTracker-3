cf998816b13d94492a70a36e7a938efed98fa23a
hi0826 <kkkttt555@naver.com>
2020-10-29 18:40:35 +0900

    feat : model class insert 구현
    model interface > class 로 변경
    insert 구현

--- BE/src/models/model.ts
+++ BE/src/models/model.ts
@@ -1,7 +1,9 @@
-export default interface Model {
-  insert(): void;
+import db from "../providers/database";
 
-  update(): void;
-
-  delete(): void;
+export default class Model {
+  static async insert<T>(pData: T, pTableName: string): Promise<number> {
+    const data = await db.query<T>(`INSERT INTO ${pTableName} SET ?`, pData);
+    const { insertId } = data[0];
+    return insertId;
+  }
 }
