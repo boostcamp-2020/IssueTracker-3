2d59abc32143e54e619c9012a6461176bc0cabb5
hi0826 <kkkttt555@naver.com>
2020-11-01 16:11:17 +0900

    chore : User Model 기능 확인용 코드 삭제
    User 모델 기능 확인용 코드 삭제
    #44

--- BE/src/routes/auth.ts
+++ BE/src/routes/auth.ts
@@ -24,29 +24,4 @@ router.post("/register", async (req: Request, res: Response, next: NextFunction)
   res.json(result);
 });
 
-router.patch("/register", async (req: Request, res: Response, next: NextFunction) => {
-  const user: User = {
-    id: req.body.id,
-    login_id: req.body.userID,
-    password: req.body.password,
-    img: "img2",
-    created_at: new Date(),
-  };
-  const result = await UserModel.update(user, "USER");
-  res.json(result);
-});
-
-router.delete("/register", async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.body;
-  const nid = Number(id);
-  const result = await UserModel.delete(nid, "USER");
-  res.json(result);
-});
-
-router.get("/users/:loginID", async (req: Request, res: Response, next: NextFunction) => {
-  const { loginID } = req.params;
-  const result = await UserModel.read(loginID, "USER");
-  res.json(result);
-});
-
 export = router;
