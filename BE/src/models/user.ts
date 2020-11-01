5135ac96200820eb8848145068d68b10d9959df7
hi0826 <kkkttt555@naver.com>
2020-11-01 16:57:38 +0900

    refactor : user 모델 export 변경
    user 모델 export 방식 변경
    #44

--- BE/src/models/user.ts
+++ BE/src/models/user.ts
@@ -2,12 +2,10 @@ import db from "../providers/database";
 import Model from "./model";
 import { User } from "../interfaces/user";
 
-class UserModel extends Model {
+export default class UserModel extends Model {
   static async read(pLoginID: string, pTableName: string): Promise<User> {
     const data = await db.query(`SELECT * FROM ${pTableName} WHERE login_id = ?`, pLoginID);
     const userData: User = data[0][0];
     return userData;
   }
 }
-
-export default UserModel;
