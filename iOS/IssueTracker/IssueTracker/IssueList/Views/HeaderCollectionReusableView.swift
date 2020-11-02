f03530415de08412b671442aab6f56a2f537b5bc
park <wogus3602@naver.com>
2020-11-02 16:23:11 +0900

    feat : Header Collection view 생성

---
+++ iOS/IssueTracker/IssueTracker/IssueList/Views/HeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderCollectionReusableView.swift
+//  IssueTracker
+//
+//  Created by ParkJaeHyun on 2020/11/02.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+}
