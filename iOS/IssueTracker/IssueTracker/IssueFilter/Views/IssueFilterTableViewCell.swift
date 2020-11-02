d32db102eed2d28bb9680ba51b1b1add23f2a6fc
park <wogus3602@naver.com>
2020-11-02 18:13:25 +0900

    feat : IssueFilter File 생성

---
+++ iOS/IssueTracker/IssueTracker/IssueFilter/Views/IssueFilterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IssueFilterTableViewCell.swift
+//  IssueTracker
+//
+//  Created by ParkJaeHyun on 2020/11/02.
+//
+
+import UIKit
+
+class IssueFilterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
