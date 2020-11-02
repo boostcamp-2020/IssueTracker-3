fa164abe76cdc99beb9d0f5ee11210899408b473
park <wogus3602@naver.com>
2020-11-02 22:31:39 +0900

    feat : IssueDetailPage CollectionView 추가

---
+++ iOS/IssueTracker/IssueTracker/IssueDetail/Views/IssueDetailCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IssueDetailCollectionViewCell.swift
+//  IssueTracker
+//
+//  Created by ParkJaeHyun on 2020/11/02.
+//
+
+import UIKit
+
+class IssueDetailCollectionViewCell: UICollectionViewCell {
+    
+}
