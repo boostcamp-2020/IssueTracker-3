c5f70298ac9d3ccc26acde1440188175e354e55d
park <wogus3602@naver.com>
2020-11-02 21:56:34 +0900

    feat : tableview에 관한 header view파일

--- iOS/IssueTracker/IssueTracker/IssueFilter/Views/HeaderView.swift
+++ iOS/IssueTracker/IssueTracker/IssueFilter/Views/HeaderView.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class HeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    init(label: UILabel) {
+        super.init(frame: .zero)
+        
+        self.addSubview(label)
 
+        NSLayoutConstraint.activate([
+            label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 5),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16)
+        ])
+    }
+
+    convenience init(text: String) {
+        let uiLabel = UILabel().makeLabel(text: text)
+        uiLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.init(label: uiLabel)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+extension UILabel {
+    func makeLabel(text: String) -> UILabel {
+        let uiLabel = UILabel()
+        uiLabel.text = text
+        return uiLabel
+    }
 }
