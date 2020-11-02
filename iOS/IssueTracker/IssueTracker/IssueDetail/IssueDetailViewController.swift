79b91890d7bef0294211a308f926d8a4faf8c31f
park <wogus3602@naver.com>
2020-11-02 22:32:38 +0900

    feat : IssueDetailViewController 추가

--- iOS/IssueTracker/IssueTracker/IssueDetail/IssueDetailViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueDetail/IssueDetailViewController.swift
@@ -11,5 +11,37 @@ class IssueDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationItem()
+    }
+
+    func configureNavigationItem() {
+        let editButtonItem = UIBarButtonItem(title: "Edit",
+                                             style: .plain,
+                                             target: nil,
+                                             action: #selector(editButtonTouched))
+        navigationItem.rightBarButtonItem = editButtonItem
+    }
+
+    @objc func editButtonTouched() {
+
+    }
+}
+
+extension IssueDetailViewController: UICollectionViewDelegate {
+
+}
+
+extension IssueDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "IssueDetailCell", for: indexPath)
     }
 }
