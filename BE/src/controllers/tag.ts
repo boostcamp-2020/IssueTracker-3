683bb6dae5aefa7b5d703aba145464e3b83b09d4
hi0826 <kkkttt555@naver.com>
2020-11-03 17:17:18 +0900

    feat : tag controller 수정
    tag api 동작변경에 따른 controller 수정
    #89

--- BE/src/controllers/tag.ts
+++ BE/src/controllers/tag.ts
@@ -1,16 +1,42 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 import { Request, Response } from "express";
 import TagModel from "@models/tag";
+import { Tag } from "@interfaces/tag";
 
-const add = async (req: Request, res: Response): Promise<Response> => {
-  const tag = { id: null, issue_id: req.body.issueId, label_id: req.body.labelId };
-  const result = await TagModel.add(tag);
+const get = async (req: Request, res: Response): Promise<Response> => {
+  const result = await TagModel.select(+req.params.issue_id);
   return res.json(result);
 };
 
-const del = async (req: Request, res: Response): Promise<Response> => {
-  const { id } = req.body;
-  const result = await TagModel.del(id);
-  return res.json(result);
-};
+const edit = async (req: Request, res: Response): Promise<Response> => {
+  const data = await TagModel.select(+req.params.issue_id);
+  const ids = data.map((value) => Number(value.id));
+  for (const id of ids) {
+    try {
+      await TagModel.del(id);
+    } catch (err) {
+      return res.sendStatus(400);
+    }
+  }
+  const issueId = Number(req.params.issueid);
+  const objs = req.body.tags.map((value: number) => {
+    const tag: Tag = {
+      id: null,
+      issue_id: issueId,
+      label_id: value,
+    };
+    return tag;
+  });
 
-export default { add, del };
+  for (const assignee of objs) {
+    try {
+      await TagModel.add(assignee);
+    } catch (err) {
+      return res.sendStatus(400);
+    }
+  }
+
+  return res.sendStatus(200);
+};
+export default { get, edit };
