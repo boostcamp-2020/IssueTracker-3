a3a98aae1c6dd679e888d3ea4483250fb9aba3f7
hi0826 <kkkttt555@naver.com>
2020-11-03 13:18:40 +0900

    feat : event 모델 추가
    event 모델 추가
    get, add 추가
    그 외의 메서드 필요 없음
    #52

--- BE/src/models/event.ts
+++ BE/src/models/event.ts
@@ -2,3 +2,29 @@ import db from "@providers/database";
 import Model from "@models/model";
 import { Event } from "@interfaces/event";
 
+class EventModel extends Model {
+  protected tableName: string;
+
+  constructor() {
+    super();
+    this.tableName = "EVENT";
+  }
+
+  async select<T>(pData: T): Promise<Array<Event>> {
+    try {
+      const result = await db.query<T>(`SELECT * FROM ${this.tableName} WHERE issue_id = ?`, pData);
+      this.data = [...result[0]];
+      return this.data;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+
+  async add(pData: Event): Promise<number> {
+    this.data = await super.insert(pData, this.tableName);
+    return this.data;
+  }
+}
+
+export default new EventModel();
