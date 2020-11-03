45410b7c851b5c143d1fd1b6256c72f2e33e85f1
hi0826 <kkkttt555@naver.com>
2020-11-03 17:15:52 +0900

    feat : assignee controller
    assignee controller 제작
    get, edit
    #91

---
+++ BE/src/controllers/assignee.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import { Request, Response } from "express";
+import AssigneeModel from "@models/assignee";
+import { Assignee } from "@interfaces/assignee";
+
+const get = async (req: Request, res: Response): Promise<Response> => {
+  const result = await AssigneeModel.select(+req.params.issue_id);
+  return res.json(result);
+};
+
+const edit = async (req: Request, res: Response): Promise<Response> => {
+  const data = await AssigneeModel.select(+req.params.issue_id);
+  const ids = data.map((value) => Number(value.id));
+  for (const id of ids) {
+    try {
+      await AssigneeModel.del(id);
+    } catch (err) {
+      return res.sendStatus(400);
+    }
+  }
+  const issueId = Number(req.params.issueid);
+  const objs = req.body.assignees.map((value: number) => {
+    const assignee: Assignee = {
+      id: null,
+      issue_id: issueId,
+      user_id: value,
+    };
+    return assignee;
+  });
+
+  for (const assignee of objs) {
+    try {
+      await AssigneeModel.add(assignee);
+    } catch (err) {
+      return res.sendStatus(400);
+    }
+  }
+
+  return res.sendStatus(200);
+};
+
+export default { get, edit };
