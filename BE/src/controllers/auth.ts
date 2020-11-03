8d431985e5053749bef615070aef66a1f3fb7963
mononomo <hostdog01@naver.com>
2020-11-03 11:28:15 +0900

    feat : 모든 로그인에 jwt 추가 밑 반환값 통일 로그인 API 를 사용할 때 모든 반환값의 형식을 통일해주었습니다.  #7

--- BE/src/controllers/auth.ts
+++ BE/src/controllers/auth.ts
@@ -7,37 +7,54 @@ import path from "path";
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 function login(req: Request, res: Response): void {
-  passport.authenticate("local", (err, userResult) => {
+  passport.authenticate("local", (err, userResult): Response<JSON> | Response<string> | undefined => {
     if (err || !userResult) {
       return res.status(400).json({
         message: "Something is not right",
-        user: userResult,
       });
     }
     req.login(userResult, (error) => {
       if (error) {
         return res.send(error);
       }
-      const accessToken = jwt.sign(JSON.parse(JSON.stringify(userResult)), String(process.env.JWT_SECRET), { expiresIn: "10m" });
-      return res.json({ userResult, accessToken });
+      const JWT = jwt.sign(JSON.parse(JSON.stringify(userResult)), String(process.env.JWT_SECRET), { expiresIn: "10m" });
+      return res.json({ state: "success", JWT });
     });
+    return res.json({ state: "fail" });
   })(req, res);
 }
-function logout(req: Request, res: Response): any {
+function apple(req: Request, res: Response): Response<JSON> | Response<string> {
+  const userResult = req.body.profile.username;
+  req.login(userResult, (error) => {
+    if (error) {
+      return res.send(error);
+    }
+    const JWT = jwt.sign(JSON.parse(JSON.stringify(userResult)), String(process.env.JWT_SECRET), { expiresIn: "10m" });
+    return res.json({ state: "success", JWT });
+  });
+  return res.json({ state: "fail" });
+}
+function githubLogin(req: Request, res: Response): Response<JSON> | Response<string> {
+  const loginUser: any = req.user;
+  req.login(loginUser, (error) => {
+    if (error) {
+      return res.send(error);
+    }
+    const decoded: any = jwt.decode(loginUser.identity_token);
+    const userEmail: string = decoded.payload.email;
+    const userResult = userEmail.split("@")[0];
+    const JWT = jwt.sign(JSON.parse(JSON.stringify(userResult)), String(process.env.JWT_SECRET), { expiresIn: "10m" });
+    return res.json({ state: "success", JWT });
+  });
+  return res.json({ state: "fail" });
+}
+
+function logout(req: Request, res: Response): Response<JSON> {
   req.logout();
   return res.json({ state: "success" });
 }
-function githubLogin(req: Request, res: Response): any {
-  const userResult = req.user;
-  return res.json({ state: "success", userResult });
-}
-function githubLoginFail(req: Request, res: Response): any {
+function githubLoginFail(req: Request, res: Response): Response<JSON> {
   return res.json({ state: "fail" });
 }
 const github = passport.authenticate("github", { failureRedirect: "/auth/github/loginFail" });
-function apple(req: Request, res: Response): any {
-  console.log(req.body);
-  const user = req.body;
-  return res.json({ user });
-}
 export default { login, logout, githubLogin, githubLoginFail, github, apple };
