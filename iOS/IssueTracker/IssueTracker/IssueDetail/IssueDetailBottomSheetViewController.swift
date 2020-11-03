621954b1a80d75979c8050f4d87c11660f3fe802
Minkwan Song <minkwan1120@naver.com>
2020-11-03 15:10:53 +0900

    feat : IssueDetailBottomSheet View 생성 및 collectionView Dummy Data를 통한 동작 확인

--- iOS/IssueTracker/IssueTracker/IssueDetail/IssueDetailBottomSheetViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueDetail/IssueDetailBottomSheetViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class IssueDetailBottomSheetViewController: UIViewController {
 
+    @IBOutlet weak var issueBottomSheetCollectionView: UICollectionView!
     @IBOutlet weak var handleArea: UIView!
     @IBOutlet weak var addCommentButton: UIButton!
     @IBOutlet weak var upButtom: UIButton!
@@ -76,4 +77,200 @@ class IssueDetailBottomSheetViewController: UIViewController {
 //
 //        view.insertSubview(bluredView, at: 0)
 //    }
+    
+    enum SectionLayoutType: Int, CaseIterable {
+        case assignee, label, milestone
+        var columnCount: Int {
+            switch self {
+            case .assignee:
+                return 3
+
+            case .label:
+                return 5
+
+            case .milestone:
+                return 1
+            }
+        }
+    }
+
+    var dataSource: UICollectionViewDiffableDataSource<SectionLayoutType, Int>! = nil
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Distinct Sections"
+        configureHierarchy()
+        if #available(iOS 14.0, *) {
+            configureDataSource()
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+
+            guard let sectionLayoutType = SectionLayoutType(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutType.columnCount
+
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                 heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+            let groupHeight = columns == 1 ?
+                NSCollectionLayoutDimension.absolute(44) :
+                NSCollectionLayoutDimension.fractionalWidth(0.2)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
+    }
+    
+    func configureHierarchy() {
+//        issueBottomSheetCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+//        issueBottomSheetCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        issueBottomSheetCollectionView.backgroundColor = .systemBackground
+//        view.addSubview(issueBottomSheetCollectionView)
+//        issueBottomSheetCollectionView.delegate = self
+    }
+    @available(iOS 14.0, *)
+    func configureDataSource() {
+        
+        let listCellRegistration = UICollectionView.CellRegistration<ListCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+        }
+        
+        let textCellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+//            cell.contentView.backgroundColor = .brown
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = SectionLayoutType(rawValue: indexPath.section)! == .label ? 8 : 0
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<SectionLayoutType, Int>(collectionView: issueBottomSheetCollectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return SectionLayoutType(rawValue: indexPath.section)! == .milestone ?
+            collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: identifier) :
+            collectionView.dequeueConfiguredReusableCell(using: textCellRegistration, for: indexPath, item: identifier)
+        }
+
+        // initial data
+        let itemsPerSection = 10
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutType, Int>()
+        SectionLayoutType.allCases.forEach {
+            snapshot.appendSections([$0])
+            let itemOffset = $0.rawValue * itemsPerSection
+            let itemUpperbound = itemOffset + itemsPerSection
+            snapshot.appendItems(Array(itemOffset..<itemUpperbound))
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension IssueDetailBottomSheetViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+class TextCell: UICollectionViewCell {
+    let label = UILabel()
+    static let reuseIdentifier = "text-cell-reuse-identifier"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemnted")
+    }
+
+}
+
+extension TextCell {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+            ])
+    }
+}
+
+class ListCell: UICollectionViewCell {
+    static let reuseIdentifier = "list-cell-reuse-identifier"
+    let label = UILabel()
+    let accessoryImageView = UIImageView()
+    let seperatorView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemented")
+    }
+}
+
+extension ListCell {
+    func configure() {
+        seperatorView.translatesAutoresizingMaskIntoConstraints = false
+        seperatorView.backgroundColor = .lightGray
+        contentView.addSubview(seperatorView)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        contentView.addSubview(label)
+
+        accessoryImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(accessoryImageView)
+
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+
+        let rtl = effectiveUserInterfaceLayoutDirection == .rightToLeft
+        let chevronImageName = rtl ? "chevron.left" : "chevron.right"
+        let chevronImage = UIImage(systemName: chevronImageName)
+        accessoryImageView.image = chevronImage
+        accessoryImageView.tintColor = UIColor.lightGray.withAlphaComponent(0.7)
+
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+            label.trailingAnchor.constraint(equalTo: accessoryImageView.leadingAnchor, constant: -inset),
+
+            accessoryImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            accessoryImageView.widthAnchor.constraint(equalToConstant: 13),
+            accessoryImageView.heightAnchor.constraint(equalToConstant: 20),
+            accessoryImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+
+            seperatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            seperatorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            seperatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            seperatorView.heightAnchor.constraint(equalToConstant: 0.5)
+            ])
+    }
 }
