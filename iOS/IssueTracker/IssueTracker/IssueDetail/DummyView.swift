bede6771baf27d69946a7434f432aa3d4080174a
Minkwan Song <minkwan1120@naver.com>
2020-11-03 13:59:17 +0900

    feat : issueList Dummy View 생성

---
+++ iOS/IssueTracker/IssueTracker/IssueDetail/DummyView.swift
@@ -0,0 +1,20 @@
+//
+//  DummyView.swift
+//  IssueTracker
+//
+//  Created by 송민관 on 2020/11/03.
+//
+
+import UIKit
+
+class DummyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
