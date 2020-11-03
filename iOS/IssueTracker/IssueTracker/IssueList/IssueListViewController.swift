096bf456833b7358e15a6a8ab7490c24bf503590
Minkwan Song <minkwan1120@naver.com>
2020-11-03 20:32:11 +0900

    sytle : IssueListViewController 코드 정리, 배치 변경, 주석

--- iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
@@ -7,20 +7,22 @@
 
 import UIKit
 
-class IssueListViewController: UIViewController {
+// TODO: Activity Indicators
 
+class IssueListViewController: UIViewController {
+    
     enum Section: CaseIterable {
         case main
     }
-
-    @IBOutlet weak var issueListCollectionView: UICollectionView!
-    var dataSource: UICollectionViewDiffableDataSource<Section, IssueListViewModel>!
-    var issueListModelController: IssueListModelController!
-
+    
+    @IBOutlet private weak var issueListCollectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, IssueListViewModel>!
+    private var issueListModelController: IssueListModelController!
+    
     private lazy var issueList: [IssueListViewModel] = {
-        return generateMountains()
+        return generateIssues()
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.searchController = UISearchController(searchResultsController: nil)
@@ -30,7 +32,37 @@ class IssueListViewController: UIViewController {
         configureDataSource()
         performQuery(with: nil)
     }
+    
+    // MARK: Dummy Issue Data
+
+    private func generateIssues() -> [IssueListViewModel] {
+        var issues = [IssueListViewModel]()
+        issues.append(IssueListViewModel(title: "test1",
+                                         description: "설명",
+                                         milestone: "프로젝트1",
+                                         labels: ["label1, label2"]))
+        issues.append(IssueListViewModel(title: "test2",
+                                         description: "설명",
+                                         milestone: "프로젝트2",
+                                         labels: ["label1, label2"]))
+        issues.append(IssueListViewModel(title: "test3",
+                                         description: "설명",
+                                         milestone: "프로젝트3",
+                                         labels: ["label1, label2"]))
+        issues.append(IssueListViewModel(title: "ha",
+                                         description: "설명",
+                                         milestone: "프로젝트4",
+                                         labels: ["label1, label2"]))
+        issues.append(IssueListViewModel(title: "haha",
+                                         description: "설명",
+                                         milestone: "프로젝트5",
+                                         labels: ["label1, label2"]))
+        return issues
+    }
+
 }
+
+// MARK: CollectionView DataSource
 
 extension IssueListViewController {
     func configureDataSource() {
@@ -40,7 +72,7 @@ extension IssueListViewController {
                 collectionView, indexPath, item
             ) -> UICollectionViewCell? in
                 guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "IssueListCell", for: indexPath) as? IssueListCollectionViewCell else { return UICollectionViewCell() }
-
+                
                 cell.titleLabel.text = item.title
                 cell.issueListDescription.text = item.description
                 cell.firstLabel.titleLabel?.text = item.labels.first
@@ -51,21 +83,7 @@ extension IssueListViewController {
     }
 }
 
-extension IssueListViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        performQuery(with: searchText)
-    }
-
-    func performQuery(with filter: String?) {
-        let issueListItems = issueListModelController.filtered(with: filter ?? "",
-                                                               model: issueList).sorted { $0.title < $1.title }
-        
-        var snapshot = NSDiffableDataSourceSnapshot<Section, IssueListViewModel>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(issueListItems)
-        dataSource.apply(snapshot, animatingDifferences: true)
-    }
-}
+// MARK: UICollectionViewDelegate
 
 extension IssueListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -77,27 +95,20 @@ extension IssueListViewController: UICollectionViewDelegate {
     }
 }
 
-private func generateMountains() -> [IssueListViewModel] {
-    var issues = [IssueListViewModel]()
-    issues.append(IssueListViewModel(title: "test1",
-                                     description: "설명",
-                                     milestone: "프로젝트1",
-                                     labels: ["label1, label2"]))
-    issues.append(IssueListViewModel(title: "test2",
-                                     description: "설명",
-                                     milestone: "프로젝트2",
-                                     labels: ["label1, label2"]))
-    issues.append(IssueListViewModel(title: "test3",
-                                     description: "설명",
-                                     milestone: "프로젝트3",
-                                     labels: ["label1, label2"]))
-    issues.append(IssueListViewModel(title: "ha",
-                                     description: "설명",
-                                     milestone: "프로젝트4",
-                                     labels: ["label1, label2"]))
-    issues.append(IssueListViewModel(title: "haha",
-                                     description: "설명",
-                                     milestone: "프로젝트5",
-                                     labels: ["label1, label2"]))
-    return issues
+// MARK: UISearchBarDelegate
+
+extension IssueListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        performQuery(with: searchText)
+    }
+    
+    func performQuery(with filter: String?) {
+        let issueListItems = issueListModelController.filtered(with: filter ?? "",
+                                                               model: issueList).sorted { $0.title < $1.title }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, IssueListViewModel>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(issueListItems)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
 }
