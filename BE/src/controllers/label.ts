028c1862cb534012fbeeff7c79adfffd486906d0
hi0826 <kkkttt555@naver.com>
2020-11-05 16:42:11 +0900

    refactor : label controller 변경
    [controller] 예외처리, 코드 스타일 변경, http code 반환하도록 변경
    #71

--- BE/src/controllers/label.ts
+++ BE/src/controllers/label.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from "express";
 import LabelModel from "@models/label";
 import { Label } from "@interfaces/label";
+import HTTPCODE from "@root/magicnumber";
 
 const get = async (req: Request, res: Response): Promise<any> => {
-  const result = await LabelModel.select();
-  return res.json(result);
+  try {
+    const result = await LabelModel.select();
+    return res.json(result);
+  } catch {
+    return res.sendStatus(HTTPCODE.SERVER_ERR);
+  }
 };
 
 const add = async (req: Request, res: Response): Promise<any> => {
-  const label: Label = { id: null, name: req.body.name, description: req.body.description, color: req.body.color, created_at: new Date() };
+  const label: Label = {
+    id: null,
+    name: req.body.name,
+    description: req.body.description,
+    color: req.body.color,
+    created_at: new Date(),
+  };
   const result = await LabelModel.add(label);
-  return res.json(result);
+  return res.sendStatus(result);
 };
 const edit = async (req: Request, res: Response): Promise<any> => {
   const label = {
@@ -20,13 +31,13 @@ const edit = async (req: Request, res: Response): Promise<any> => {
     color: req.body.color,
   };
   const result = await LabelModel.edit(label);
-  return res.json(result);
+  return res.sendStatus(result);
 };
 
 const del = async (req: Request, res: Response): Promise<any> => {
   const { id } = req.body;
   const result = await LabelModel.del(id);
-  return res.json(result);
+  return res.sendStatus(result);
 };
 
 export default { get, add, edit, del };
