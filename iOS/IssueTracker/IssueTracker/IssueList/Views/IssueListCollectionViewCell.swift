1ab6b127c4ee5fb05dd8cc6812cb44ca5bfa8e84
Minkwan Song <minkwan1120@naver.com>
2020-11-08 15:49:57 +0900

    refactor : IssueListCell constraint 수정 / 불필요한 코드 정리 / edti accessories separator contraint

--- iOS/IssueTracker/IssueTracker/IssueList/Views/IssueListCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/Views/IssueListCollectionViewCell.swift
@@ -10,58 +10,39 @@ import UIKit
 final class IssueListCollectionViewCell: UICollectionViewListCell {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
-    @IBOutlet weak var labelStackView: UIStackView!
+    @IBOutlet private weak var labelStackView: UIStackView!
     
-    var isInEditingMode: Bool = false {
-        didSet {
-            toggleEditingMode()
-        }
+    var isInEditingMode: Bool = false { didSet { } } // selected 문제 해결 후 삭제 예정
+    
+    func configureIssueListCell(of item: IssueListViewModel) {
+        configureCellLabels(with: item)
+        configureLabelStackView(milestone: item.milestone, labels: item.labels)
+        configureAccessories()
     }
     
-    override var isSelected: Bool {
-        didSet {
-            if isInEditingMode {
-                backgroundColor = .systemGray4
-            }
-        }
-    }
-    
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//
-//        labelStackView.subviews.forEach({
-//            $0.removeFromSuperview()
-//        })
-//    }
-
-    fileprivate func extractedFunc(_ item: IssueListViewModel) {
+    private func configureCellLabels(with item: IssueListViewModel) {
         titleLabel.text = item.title
         descriptionLabel.text = item.description
     }
     
-    func configureIssueListCell(of item: IssueListViewModel) {
-        extractedFunc(item)
-        
-        configureLabelStackView(milestone: item.milestone, labels: item.labels)
-    }
-    
-    func configureLabelStackView(milestone: UIButton, labels: [UIButton]) {
+    // TODO: milestone, labels가 nil 이라면?
+    private func configureLabelStackView(milestone: UIButton, labels: [UIButton]) {
         labelStackView.subviews.forEach({
             $0.removeFromSuperview()
         })
-
         labelStackView.addArrangedSubview(milestone)
         labels.forEach({
             labelStackView.addArrangedSubview($0)
         })
     }
     
-    // TODO: Moving Animation
-    private func toggleEditingMode() {
-        if isInEditingMode {
-            contentView.layer.bounds.origin.x -= 40
-        } else {
-            contentView.layer.bounds.origin.x += 40
-        }
+    private func configureAccessories() {
+        accessories = [
+            .multiselect(displayed: .whenEditing, options: .init())
+        ]
+        separatorLayoutGuide
+            .trailingAnchor
+            .constraint(equalTo: titleLabel.leadingAnchor)
+            .isActive = true
     }
 }
