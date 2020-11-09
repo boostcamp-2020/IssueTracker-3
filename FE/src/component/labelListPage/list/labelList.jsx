d3e93bd2d6058289c92d884e5d1d533b5ce9b7e5
mononomo <hostdog01@naver.com>
2020-11-09 16:38:22 +0900

    fix : labelList 의 list 가 동작합니다. labelList 가 axios 로 동작합니다. #20

--- FE/src/component/labelListPage/list/labelList.jsx
+++ FE/src/component/labelListPage/list/labelList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import styled from "styled-components";
+import useRequest from "../../../util/useRequest";
 
 const StyledLabelList = styled.div`
   display: flex;
@@ -19,22 +20,32 @@ const StyledLabel = styled.div`
   margin: 5px;
 `;
 function LabelList() {
-  const animalList = ["dog", "cat", "tiger"];
+  const [response, loading, error] = useRequest("http://101.101.210.34:3000/label");
+  if (loading) {
+    return <div>로딩중..</div>;
+  }
+  if (error) {
+    return <div>에러 발생!</div>;
+  }
+  if (!response) return null;
 
+  const labels = response.data;
   return (
     <StyledLabelList>
       <StyledLabelHeader>Label List header</StyledLabelHeader>
-      {animalList.map((animal, index) => (
-        <Label key={index} name={animal} />
+      {labels.map((label, index) => (
+        <Label key={index} name={label.name} color={label.color} description={label.description} />
       ))}
     </StyledLabelList>
   );
 }
 
-function Label({ name }) {
+function Label({ name, color, description }) {
   return (
     <StyledLabel>
       <span>{name}</span>
+      <span>{color}</span>
+      <span>{description}</span>
     </StyledLabel>
   );
 }
