ef699c15ac647b9d2b5d3b24f368238772318e92
hi0826 <kkkttt555@naver.com>
2020-11-09 17:37:56 +0900

    refactor : comment
    model - iOS와의 연동을 위해서 nullfilter 추가,
    add시 insert된 id 보내도록 추가
    controller - 입력값 없을시 null로 변경
    add 변경에 따른 response 변경
    #71

--- BE/src/controllers/comment.ts
+++ BE/src/controllers/comment.ts
@@ -3,7 +3,7 @@ import CommentModel from "@models/comment";
 import { Comment } from "@interfaces/comment";
 import HTTPCODE from "@utils/magicnumber";
 
-const get = async (req: Request, res: Response): Promise<Response> => {
+const getIssueComment = async (req: Request, res: Response): Promise<Response> => {
   try {
     const result = await CommentModel.select(+req.params.issueid);
     return res.json(result);
@@ -18,11 +18,11 @@ const add = async (req: Request, res: Response): Promise<Response> => {
     issue_id: req.body.issue_id,
     user_id: req.body.user_id,
     body: req.body.body,
-    emoji: req.body.emoji,
+    emoji: req.body?.emoji ?? null,
     created_at: new Date(),
   };
   const result = await CommentModel.add(comment);
-  return res.sendStatus(result);
+  return res.status(result.httpcode).json(result.message);
 };
 
 const edit = async (req: Request, res: Response): Promise<Response> => {
@@ -42,4 +42,4 @@ const del = async (req: Request, res: Response): Promise<Response> => {
   return res.sendStatus(result);
 };
 
-export default { get, add, edit, del };
+export default { getIssueComment, add, edit, del };
