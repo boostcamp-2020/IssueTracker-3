3ee7e8b0b7836b34fede2dc5c72894ae602c316e
Minkwan Song <minkwan1120@naver.com>
2020-11-09 22:51:11 +0900

    feat : CreateIssueInteractor 프로토콜 및 클래스 틀 잡기

--- iOS/IssueTracker/IssueTracker/CreateIssue/CreateIssueInteractor.swift
+++ iOS/IssueTracker/IssueTracker/CreateIssue/CreateIssueInteractor.swift
@@ -7,40 +7,31 @@
 
 import Foundation
 
-protocol CreateIssueInteractorBusinessLogic {
-    
+protocol CreateIssueDataStore {
+    var createdIssue: Issue? { get set }
 }
 
-final class CreateIssueInteractor: CreateIssueInteractorBusinessLogic {
-    func encodeNewIssue(issue: NewIssue) -> Data? {
-        return try? JSONEncoder().encode(createdIssue)
+protocol CreateIssueBusinessLogic {
+    func uploadIssue()
+}
+
+final class CreateIssueInteractor: CreateIssueDataStore {
+    var createdIssue: Issue?
+}
+
+extension CreateIssueInteractor: CreateIssueBusinessLogic {
+    func uploadIssue() {
+        // post
+        // patch - label
     }
-
-    func uploadCreatedIssue() {
-        guard let url = URL(string: APIServer.baseURL) else {
-            return
-        }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpBody =
-        
-        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            guard error == nil else {
-                // error 처리
-                return
-            }
-            guard (200...299).contains() else {
-                result.send(completion: .failure(.error(message: "\(httpResponse.statusCode)")))
-                return
-            }
-            guard (400...499).contains(response)
-            guard let dataURL = dataURL else {
-                result.send(completion: .failure(.downloadFailed))
-                return
-            }
-
-
-        }.resume()
-        NetworkManager(sessionManager: AF).request(endPoint: CreateIssueEndPoint) { (_: Data?) in
-
-        }
 }
+
+extension Encodable {
+        
+}
+
+//guard (200...299).contains() else {
+//    result.send(completion: .failure(.error(message: "\(httpResponse.statusCode)")))
+//    return
+//}
+//guard (400...499).contains(response)
