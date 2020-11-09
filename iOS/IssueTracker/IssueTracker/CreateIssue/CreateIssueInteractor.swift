b7d9859b63ad5937f9e135cc4c32f5019eb00e41
Minkwan Song <minkwan1120@naver.com>
2020-11-10 03:56:30 +0900

    feat : CreateIssueInteractor 프로토콜 채택 & 프로퍼티 생성 & uploadIssue 1차 구현

--- iOS/IssueTracker/IssueTracker/CreateIssue/CreateIssueInteractor.swift
+++ iOS/IssueTracker/IssueTracker/CreateIssue/CreateIssueInteractor.swift
@@ -16,18 +16,31 @@ protocol CreateIssueBusinessLogic {
 }
 
 final class CreateIssueInteractor: CreateIssueDataStore {
+    let networkService: NetworkServiceProvider = NetworkService()
+    var presenter: IssueListPresentationLogic?
     var createdIssue: Issue?
 }
 
 extension CreateIssueInteractor: CreateIssueBusinessLogic {
     func uploadIssue() {
+        guard let data = try? createdIssue?.encoded() else {
+            // Error 처리
+            return
+        }
         // post
-        // patch - label
+        networkService.request(apiConfiguration: CreateIssueEndPoint.upload(data)) { [weak self] result in
+            switch result {
+            case .failure(_):
+                print("create issue 실패")
+                // Error 처리
+                return
+            case .success(let _):
+                // 성공 처리
+                return
+            }
+        }
+        // patch - label 등 나머지
     }
-}
-
-extension Encodable {
-        
 }
 
 //guard (200...299).contains() else {
