449085be600e36300d6c845dce1fb7938475fe3d
Minkwan Song <minkwan1120@naver.com>
2020-11-09 20:54:17 +0900

    style : UserInfo Entity 파일 분리

--- iOS/IssueTracker/IssueTracker/Entity/UserInfo.swift
+++ iOS/IssueTracker/IssueTracker/Entity/UserInfo.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct UserInfo: Codable {
+    let state: String
+    let token: String
+    let userID: String
+    
+    enum CodingKeys: String, CodingKey {
+        case state
+        case token = "JWT"
+        case userID = "user_id"
+    }
+}
