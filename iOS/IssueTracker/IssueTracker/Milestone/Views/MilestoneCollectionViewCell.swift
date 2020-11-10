3151250eb45c33184777566f834d432ba7efeeea
Minkwan Song <minkwan1120@naver.com>
2020-11-11 04:16:06 +0900

    feat : Milestone Cell IBOutlet 연결 / custom class 설정

--- iOS/IssueTracker/IssueTracker/Milestone/Views/MilestoneCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/Milestone/Views/MilestoneCollectionViewCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class MilestoneCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet private weak var milestoneStackView: UIStackView!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var dueDateLabel: UILabel!
+    @IBOutlet private weak var percentageLabel: UILabel!
+    @IBOutlet private weak var openIssuesCountLabel: UILabel!
+    @IBOutlet private weak var closedIssuesCountLabel: UILabel!
+    
     func configure(viewModel: MilestoneViewModel) {
        
     }
