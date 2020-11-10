ce0a866ca365d4cc3215d91cf1cd7327b547cb67
Minkwan Song <minkwan1120@naver.com>
2020-11-11 04:05:22 +0900

    feat : MilestoneInteractor 생성 및 DataStore/BusinessLogic Protocol 및 fetchMilestones() 함수 구현

--- iOS/IssueTracker/IssueTracker/Milestone/MilestoneInterator.swift
+++ iOS/IssueTracker/IssueTracker/Milestone/MilestoneInterator.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+protocol MilestoneDataStore {
+    var milestones: MilestoneList { get set }
+}
+
+protocol MilestoneBusinessLogic {
+    func fetchMilestones()
+}
+
+final class MilestoneInteractor: MilestoneDataStore {
+    let networkService: NetworkServiceProvider = NetworkService()
+    var presenter: MilestonePresentationLogic?
+    var milestones = MilestoneList()
+}
+
+extension MilestoneInteractor: MilestoneBusinessLogic {
+    func fetchMilestones() {
+        networkService.request(apiConfiguration: MilestoneEndPoint.getMilestones) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .failure(let error):
+                debugPrint(error)
+                return
+            case .success(let data):
+                guard let decodedData: MilestoneList = try? data.decoded() else {
+                    debugPrint("decode 실패")
+                    return
+                }
+                self.milestones = decodedData
+                self.presenter?.presentFetchedMilestones(milestones: self.milestones)
+            }
+        }
+    }
+}
