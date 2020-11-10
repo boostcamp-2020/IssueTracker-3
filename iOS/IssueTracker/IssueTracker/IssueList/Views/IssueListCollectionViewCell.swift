bd6ddb454e278d59ef09e4f97103d1e4dca2bc1e
park <wogus3602@naver.com>
2020-11-10 10:33:28 +0900

    chore : configure 이름 변경

--- iOS/IssueTracker/IssueTracker/IssueList/Views/IssueListCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/Views/IssueListCollectionViewCell.swift
@@ -14,7 +14,7 @@ final class IssueListCollectionViewCell: UICollectionViewListCell {
     
     var isInEditingMode: Bool = false { didSet { } } // selected 문제 해결 후 삭제 예정
     
-    func configureIssueListCell(of item: IssueListViewModel) {
+    func configure(of item: IssueListViewModel) {
         configureCellLabels(with: item)
         configureLabelStackView(milestone: item.milestone, labels: item.labels)
         configureAccessories()
