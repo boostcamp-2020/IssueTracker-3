b8e1a7478c50698ca5fbcc2995f2ae1953b11e7a
Minkwan Song <minkwan1120@naver.com>
2020-11-11 18:11:33 +0900

    feat : MilestoneCalculator reset() 함수 구현 및 딕셔너리 저장 변수명 (open/closed IssueCounter)로 변경

--- iOS/IssueTracker/IssueTracker/Milestone/Models/MilestoneCalculator.swift
+++ iOS/IssueTracker/IssueTracker/Milestone/Models/MilestoneCalculator.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct MilestoneCalculator {
+    static var openIssuesCounter = [Int: Int]()
+    static var closedIssuesCounter = [Int: Int]()
+    
+    enum IssueState {
+        case open
+        case closed
+    }
+    
+    static subscript (id: Int, state: IssueState) -> Int {
+        switch state {
+        case .open:
+            return MilestoneCalculator.openIssuesCounter[id] ?? 0
+        case .closed:
+            return MilestoneCalculator.closedIssuesCounter[id] ?? 0
+        }
+    }
+    
+    static func input(milestoneID: MilestoneID, issueState: Int) {
+        var key = -1
+        switch milestoneID {
+        case .integer(let id):
+            key = id
+        case .string(_:):
+            return
+        }
+
+        switch issueState {
+        case 0:
+            MilestoneCalculator.closedIssuesCounter[key, default: 0] += 1
+        case 1:
+            MilestoneCalculator.openIssuesCounter[key, default: 0] += 1
+        default:
+            return
+        }
+    }
+    
+    static func reset() {
+        MilestoneCalculator.openIssuesCounter.removeAll()
+        MilestoneCalculator.closedIssuesCounter.removeAll()
+    }
+}
