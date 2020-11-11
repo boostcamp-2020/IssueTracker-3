596cfc04513f103b03a8de7d9b3a7d0206383420
Minkwan Song <minkwan1120@naver.com>
2020-11-12 05:01:25 +0900

    feat : UIVC hideKeyboardWhenTappedAround() 구현

--- iOS/IssueTracker/IssueTracker/Common/Extension/UIViewController+hideKeyboardWhenTapped.swift
+++ iOS/IssueTracker/IssueTracker/Common/Extension/UIViewController+hideKeyboardWhenTapped.swift
@@ -1,8 +1,28 @@
 //
-//  UIViewController+ hideKeyboardWhenTappedAround.swift
+//  UIViewController+hideKeyboard.swift
 //  IssueTracker
 //
 //  Created by 송민관 on 2020/11/12.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    /// TextField 등 editing 상태에서 다른 아무 곳을 tap하면 keyboard를 숨김
+    ///```
+    /// override func viewDidLoad() {
+    ///    super.viewDidLoad()
+    ///    hideKeyboardWhenTappedAround()
+    /// }
+    ///```
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self,
+                                         action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
