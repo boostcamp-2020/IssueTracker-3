7aca9b6c1e810c435312041532b11befcfa4f97e
park <wogus3602@naver.com>
2020-11-13 03:14:26 +0900

    feat : AllGetUser 네트워크 모델 추가

--- iOS/IssueTracker/IssueTracker/Entity/AllGetUser.swift
+++ iOS/IssueTracker/IssueTracker/Entity/AllGetUser.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct AllGetUser: Codable {
+    let id: Int
+    let loginID, password: String
+    let img: String
+    let createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case loginID = "login_id"
+        case password, img
+        case createdAt = "created_at"
+    }
+}
+
+typealias AllGetUserList = [AllGetUser]
